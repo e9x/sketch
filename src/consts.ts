157470926d832aa0fb168a92b50d2b8028e71423
David Reed <reedswork@proton.me>
2023-04-14 01:04:12 -0400

    SKETCH_

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 export const apiURL = process.env.SKETCH_API_URL || "";
-if (!apiURL) throw new TypeError("Invalid API_URL");
+if (!apiURL) throw new TypeError("Invalid SKETCH_API_URL");
 
 export const workInkURL = process.env.SKETCH_WORKINK_URL || "";
-if (!workInkURL) throw new TypeError("Invalid API_URL");
+if (!workInkURL) throw new TypeError("Invalid SKETCH_API_URL");
 
 export const gameVersion = process.env.SKETCH_GAME_VERSION || "";
-if (!gameVersion) throw new TypeError("Invalid GAME_VERSION");
+if (!gameVersion) throw new TypeError("Invalid SKETCH_GAME_VERSION");
 
 export function getDevURL() {
   const devHost = process.env.SKETCH_DEV_HOST || "";
