bd3c48eb983e8a342d8cdee25247f4453df3ee00
David Reed <reedswork@proton.me>
2023-04-15 15:31:59 -0400

    add lodash, pick random point for slidehop

--- src/cheats/bhop.tsx
+++ src/cheats/bhop.tsx
@@ -2,11 +2,17 @@ import useConfig, { configGet } from "../config";
 import { iInputs } from "../consts";
 import { getLocalPlayer, inputHooks } from "../filters";
 import Switch from "../menu/components/Switch";
+import random from "lodash/random";
 
 const defaultBhop = false;
 
 export function bhopHook() {
   let lastJump = 0;
+  // value between -1 and 1 that determines the velocity to start slidehopping at
+  // positive = going down
+  // negative = still increasing in velocity
+  let zeroSome = 0;
+  let didCrouch = false;
 
   inputHooks.push((inputs) => {
     if (!configGet("bhop", defaultBhop)) return;
@@ -26,8 +32,18 @@ export function bhopHook() {
       // if(!inputs[iInputs.crouch])
 
       // if crouch is held, slidehop
-      if (inputs[iInputs.crouch])
-        inputs[iInputs.crouch] = (localPlayer.velocity.y || 0) < 0 ? 1 : 0;
+      if (inputs[iInputs.crouch]) {
+        // pick a new "zeroSome" everytime we slidehop
+        // otherwise they will randomly start/stop crouching as zeroSome is recalculated
+        // the users will appear to be relatively accurate with slidehopping
+        if (!didCrouch) zeroSome = random(-0.015, 0.005, true);
+
+        inputs[iInputs.crouch] =
+          (localPlayer.velocity.y || 0) < zeroSome ? 1 : 0;
+        if (inputs[iInputs.crouch]) didCrouch = true;
+      } else {
+        didCrouch = false;
+      }
     }
   });
 }
