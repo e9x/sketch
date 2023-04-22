1f9a59597c30ad2ab204c2f3ac5f26c6101758f5
David Reed <reedswork@proton.me>
2023-04-22 14:48:55 -0400

    triggerbot key

--- src/cheats/triggerbot.tsx
+++ src/cheats/triggerbot.tsx
@@ -2,17 +2,27 @@ import useConfig, { configGet } from "../config";
 import { iInputs } from "../consts";
 import { getGame, getRender, inputHooks } from "../filters";
 import { isEnemy } from "../krunkerUtil";
+import BindHolder, { Bind } from "../menu/components/Bind";
 import Switch from "../menu/components/Switch";
 
 const defaultTriggerbot = false;
+const defaultTriggerbotKey = -1;
 
 export function triggerbotHook() {
   inputHooks.push((inputs) => {
     if (!configGet<boolean>("triggerbot", defaultTriggerbot)) return;
 
+    const game = getGame();
+
+    const triggerbotKey = configGet<number>(
+      "triggerbotKey",
+      defaultTriggerbotKey
+    );
+
+    if (triggerbotKey !== -1 && game.controls.keys[triggerbotKey] !== 1) return;
+
     if (!inputs[iInputs.scope]) return;
 
-    const game = getGame();
     const render = getRender();
 
     const direction = new game.THREE.Vector3();
@@ -38,17 +48,31 @@ export function triggerbotHook() {
 }
 
 export function TriggerbotMenu() {
-  const [triggerbot, setTriggerbot] = useConfig(
+  const [triggerbot, setTriggerbot] = useConfig<boolean>(
     "triggerbot",
     defaultTriggerbot
   );
+  const [triggerbotKey, setTriggerbotKey] = useConfig<number>(
+    "triggerbotKey",
+    defaultTriggerbotKey
+  );
 
   return (
-    <Switch
-      title="Triggerbot"
-      description="Shoots enemys that come into your line of sight while you're holding right click"
-      defaultChecked={triggerbot}
-      onChange={(event) => setTriggerbot(event.currentTarget.checked)}
-    />
+    <>
+      <BindHolder title="Triggerbot Key">
+        <Bind
+          bind={triggerbotKey}
+          setBind={(bind) => setTriggerbotKey(bind)}
+          reset={() => setTriggerbotKey(null)}
+          unbind={() => setTriggerbotKey(-1)}
+        />
+      </BindHolder>
+      <Switch
+        title="Triggerbot"
+        description="Shoots enemys that come into your line of sight while you're holding right click"
+        defaultChecked={triggerbot}
+        onChange={(event) => setTriggerbot(event.currentTarget.checked)}
+      />
+    </>
   );
 }
