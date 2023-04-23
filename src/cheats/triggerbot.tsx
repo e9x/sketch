9752d76905c7d71137355e8fe51ce9dc421e5a93
David Reed <reedswork@proton.me>
2023-04-22 21:18:37 -0400

    make min/max configurable

--- src/cheats/triggerbot.tsx
+++ src/cheats/triggerbot.tsx
@@ -3,14 +3,18 @@ import { iInputs } from "../consts";
 import { getGame, getRender, inputHooks } from "../filters";
 import { isEnemy } from "../krunkerUtil";
 import BindHolder, { Bind } from "../menu/components/Bind";
+import Slider from "../menu/components/Slider";
 import Switch from "../menu/components/Switch";
 
 const defaultTriggerbot = false;
 const defaultTriggerbotKey = -1;
+const defaultTriggerbotMin = 0;
+const defaultTriggerbotMax = 0;
 
 export function triggerbotHook() {
   let detectTime = 0;
-  const detectDelay = 200;
+  let continueTime = 0;
+  let didShoot = false;
 
   inputHooks.push((inputs) => {
     if (!configGet<boolean>("triggerbot", defaultTriggerbot)) return;
@@ -26,42 +30,62 @@ export function triggerbotHook() {
       (triggerbotKey === -1 || game.controls.keys[triggerbotKey] === 1) &&
       inputs[iInputs.scope]
     ) {
-      const render = getRender();
+      if (Date.now() < continueTime) inputs[iInputs.shoot] = 1;
+      else {
+        const render = getRender();
 
-      const direction = new game.THREE.Vector3();
-      const position = new game.THREE.Vector3();
+        const direction = new game.THREE.Vector3();
+        const position = new game.THREE.Vector3();
 
-      render.camera.getWorldDirection(direction);
-      render.camera.getWorldPosition(position);
+        render.camera.getWorldDirection(direction);
+        render.camera.getWorldPosition(position);
 
-      game.raycaster.set(position, direction);
+        game.raycaster.set(position, direction);
 
-      let shoot = false;
+        let shoot = false;
 
-      for (const player of game.players.list)
-        if (
-          isEnemy(player) &&
-          player.objInstances &&
-          player.canBSeen &&
-          game.raycaster.intersectObjects(player.objInstances.children, true)
-            .length
-        ) {
-          shoot = true;
-          break;
+        for (const player of game.players.list)
+          if (
+            isEnemy(player) &&
+            player.objInstances &&
+            player.canBSeen &&
+            game.raycaster.intersectObjects(player.objInstances.children, true)
+              .length
+          ) {
+            shoot = true;
+            break;
+          }
+
+        if (shoot) {
+          if (!detectTime) {
+            console.log("just detected!?");
+            // Date.now() + detectDelay
+            detectTime =
+              Date.now() +
+              configGet<number>("triggerbotMin", defaultTriggerbotMin) * 1000;
+          }
+
+          if (detectTime < Date.now()) {
+            console.log("shoot everywhere");
+            inputs[iInputs.shoot] = 1;
+          }
+
+          didShoot = true;
+        } else if (didShoot) {
+          detectTime = 0;
+          console.log("no shoot");
+          // Date.now() + continueFor
+          continueTime =
+            Date.now() +
+            configGet<number>("triggerbotMax", defaultTriggerbotMax) * 1000;
+          didShoot = false;
         }
-
-      if (shoot) {
-        if (!detectTime) {
-          console.log("just detected!?");
-          detectTime = Date.now() + detectDelay;
-        }
-
-        if (detectTime < Date.now()) {
-          console.log("shoot everywhere");
-          inputs[iInputs.shoot] = 1;
-        }
-      } else detectTime = 0;
-    } else detectTime = 0;
+      }
+    } else {
+      detectTime = 0;
+      continueTime = 0;
+      didShoot = false;
+    }
   });
 }
 
@@ -73,6 +97,14 @@ export function TriggerbotMenu() {
   const [triggerbotKey, setTriggerbotKey] = useConfig<number>(
     "triggerbotKey",
     defaultTriggerbotKey
+  );
+  const [triggerbotMin, setTriggerbotMin] = useConfig<number>(
+    "triggerbotMin",
+    defaultTriggerbotMin
+  );
+  const [triggerbotMax, setTriggerbotMax] = useConfig<number>(
+    "triggerbotMax",
+    defaultTriggerbotMax
   );
 
   return (
@@ -90,6 +122,26 @@ export function TriggerbotMenu() {
         description="Shoots enemys that come into your line of sight while you're holding right click"
         defaultChecked={triggerbot}
         onChange={(event) => setTriggerbot(event.currentTarget.checked)}
+      />
+      <Slider
+        title="Triggerbot Minimum (Seconds)"
+        defaultValue={triggerbotMin}
+        onChange={(event) =>
+          setTriggerbotMin(event.currentTarget.valueAsNumber)
+        }
+        min={0}
+        max={1}
+        step={0.05}
+      />
+      <Slider
+        title="Triggerbot Maximum (Seconds)"
+        defaultValue={triggerbotMax}
+        onChange={(event) =>
+          setTriggerbotMax(event.currentTarget.valueAsNumber)
+        }
+        min={0}
+        max={1}
+        step={0.05}
       />
     </>
   );
