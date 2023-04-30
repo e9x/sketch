c0e18cf21c4917254552b185c740194a8ca0c312
David Reed <reedswork@proton.me>
2023-04-29 20:12:37 -0400

    treat rotation like rotation

--- src/tracker.tsx
+++ src/tracker.tsx
@@ -239,6 +239,48 @@ function hookGame(game: Game) {
   };
 }
 
+function normalizeAngle(angle: number) {
+  while (angle < 0) {
+    angle += Math.PI * 2;
+  }
+  while (angle >= Math.PI * 2) {
+    angle -= Math.PI * 2;
+  }
+  return angle;
+}
+
+function rotationDifference(
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+): { deltaX: number; deltaY: number } {
+  // Normalize the input angles to the range [0, Math.PI * 2)
+  x1 = normalizeAngle(x1);
+  y1 = normalizeAngle(y1);
+  x2 = normalizeAngle(x2);
+  y2 = normalizeAngle(y2);
+
+  // Calculate the difference between the angles
+  let deltaX = x2 - x1;
+  let deltaY = y2 - y1;
+
+  // Correct the differences to be within the range [-Math.PI, Math.PI)
+  if (deltaX > Math.PI) {
+    deltaX -= Math.PI * 2;
+  } else if (deltaX < -Math.PI) {
+    deltaX += Math.PI * 2;
+  }
+
+  if (deltaY > Math.PI) {
+    deltaY -= Math.PI * 2;
+  } else if (deltaY < -Math.PI) {
+    deltaY += Math.PI * 2;
+  }
+
+  return { deltaX, deltaY };
+}
+
 function hookInputs(inputs: number[]) {
   if (lastInputs) {
     const x1 = inputs[iInputs.xDir] / 1000;
@@ -247,7 +289,9 @@ function hookInputs(inputs: number[]) {
     const x2 = lastInputs[iInputs.xDir] / 1000;
     const y2 = lastInputs[iInputs.yDir] / 1000;
 
-    const distance = Math.hypot(x2 - x1, y2 - y1);
+    const delta = rotationDifference(x1, y1, x2, y2);
+
+    const distance = Math.hypot(delta.deltaX, delta.deltaY);
 
     const flags =
       (inputs[iInputs.shoot] === 1 ? InputFlags.shoot : 0) |
