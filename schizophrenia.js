76986932301ea53d07578fc14a625d5063cc77ed
David Reed <reedswork@proton.me>
2023-05-07 15:38:58 -0400

    better schizo types

--- schizophrenia.js
+++ schizophrenia.js
@@ -3,12 +3,20 @@ import sample from "lodash/sample.js";
 const firstChars = ["i", "I"];
 const secondChars = ["î", "i", "ï", "í", "ì"];
 
-/**
- *
- * @returns {string}
- */
 export function generateIdentifier() {
+  /**
+   * @type {string}
+   */
   let id = sample(firstChars);
-  for (let i = 0; i < 6; i++) id += sample(secondChars);
+
+  for (let i = 0; i < 6; i++) {
+    /**
+     * @type {string}
+     */
+    const second = sample(secondChars);
+
+    id += second;
+  }
+
   return id;
 }
