3675c14bd04fe1d550229379927b802585fd4c65
David Reed <reedswork@proton.me>
2023-05-15 18:10:09 -0400

    timeout

--- src/linkvertise.ts
+++ src/linkvertise.ts
@@ -2,8 +2,13 @@ import { linkvertisePage } from "./consts";
 import tokenConfig from "./tokenConfig";
 
 // they went to the linkvertise
+
+// wait a little bit
+// like right after the bypasser redirects them
 if (location.toString().startsWith(linkvertisePage))
-  tokenConfig.set("lv", true);
+  setTimeout(() => {
+    tokenConfig.set("lv", true);
+  }, 1e3);
 
 // export a highly obfuscated function
 export function wentToLV() {
