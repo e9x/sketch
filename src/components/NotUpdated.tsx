f3b021514a26ca22c6323acfb0f69422a98f133a
David Reed <reedswork@proton.me>
2023-05-22 17:01:40 -0400

    more clear message

--- src/components/NotUpdated.tsx
+++ src/components/NotUpdated.tsx
@@ -3,8 +3,15 @@ import { discordURL } from "../consts";
 export default function NotUpdated() {
   return (
     <>
-      <h1>Sketch isn't updated.</h1>
-      <a href={discordURL}>Discord server</a>
+      <h1>Sketch is outdated and an update isn't available.</h1>
+      <hr />
+      <p>You'll have to wait for an update.</p>
+      <p style={{ fontSize: "0.6em" }}>
+        <em>Sketch has to be updated every time Krunker updates.</em>
+      </p>
+      <p>
+        <a href={discordURL}>Discord server</a>
+      </p>
     </>
   );
 }
