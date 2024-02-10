233b109a919149be171dc2b90ed1507e39e8f9b7
David Reed <reedswork@proton.me>
2024-02-10 14:18:24 -0500

    rework skinhack, face support

--- src/cheats/skins.tsx
+++ src/cheats/skins.tsx
@@ -9,17 +9,7 @@ import type { Skin } from "../krunker/Player";
 import sketchConfig, { useSketchConfig } from "../sketchConfig";
 import { Switch } from "krunker-ui/components/Switch";
 
-interface SkinHackSkins {
-  main: number;
-  secondary: number;
-  hat: number;
-  body: number;
-  knife: number;
-  dye: number;
-  waist: number;
-}
-
-let skinData: SkinHackSkins | undefined;
+let skinData: Record<string, number> | undefined;
 
 export function skinHackHook() {
   const dummySkinArray = [...Array(25000)].map((_, i) => ({
@@ -28,6 +18,15 @@ export function skinHackHook() {
   })) as Skin[];
 
   const realSkins = new WeakMap<Player, Skin[]>();
+
+  function resolveSkin(id: number) {
+    const menuPlayer = getMenuPlayer();
+    if (!menuPlayer) return;
+    const skins = realSkins.get(menuPlayer) || menuPlayer.skins;
+
+    // remove skins if they don't have it
+    return id === -1 || skins.find(({ ind }) => ind === id) ? -1 : id;
+  }
 
   playerConstructorHooks.push((player) => {
     realSkins.set(player, player.skins);
@@ -45,37 +44,30 @@ export function skinHackHook() {
 
   ioSendHooks.push((packet, data) => {
     if (packet === "en" && sketchConfig.get("skinHack")) {
+      const skins = data[0];
+
       skinData = {
-        main: data[0][2][0],
-        secondary: data[0][2][1],
-        hat: data[0][3],
-        body: data[0][4],
-        knife: data[0][9],
-        dye: data[0][14],
-        waist: data[0][17],
+        main: skins[2][0],
+        secondary: skins[2][1],
+        hat: skins[3],
+        body: skins[4],
+        knife: skins[9],
+        dye: skins[14],
+        shoe: skins[15],
+        waist: skins[17],
+        face: skins[20],
+        pet: skins[21],
+        wrist: skins[25],
+        charm: skins[29][0],
+        charms: skins[29][1],
       };
 
-      const menuPlayer = getMenuPlayer();
-      if (!menuPlayer) return;
-      const skins = realSkins.get(menuPlayer) || menuPlayer.skins;
+      for (const i in [0, 1]) skins[2][i] = resolveSkin(skins[2][i]);
 
-      // remove skins if they don't have it
+      for (const i of [3, 4, 9, 14, 15, 17, 21, 25])
+        skins[i] = resolveSkin(skins[i]);
 
-      // primary, secondary
-      for (const skinId in [0, 1])
-        if (
-          data[0][2][skinId] !== -1 &&
-          !skins.find(({ ind }) => ind === data[0][2][skinId])
-        )
-          data[0][2][skinId] = -1;
-
-      // hat, body, knife, dye, waist
-      for (const skinId of [3, 4, 9, 14, 17])
-        if (
-          data[0][skinId] !== -1 &&
-          !skins.find(({ ind }) => ind === data[0][skinId])
-        )
-          data[0][skinId] = -1;
+      for (const i in [0, 1]) skins[29][i] = resolveSkin(skins[29][i]);
     }
   });
 
@@ -86,17 +78,23 @@ export function skinHackHook() {
       if (!menuPlayer) return;
 
       const skinValue = data[0][0];
-      let offset = 38;
+      let offset = 46;
       while (skinValue.length % offset !== 0) offset++;
 
       for (let i = 0; i < skinValue.length; i += offset) {
+        console.log(skinValue[i + 5]);
         if (skinValue[i + 5] === menuPlayer.name) {
           skinValue[i + 12] = [skinData.main, skinData.secondary];
           skinValue[i + 13] = skinData.hat;
           skinValue[i + 14] = skinData.body;
           skinValue[i + 19] = skinData.knife;
           skinValue[i + 24] = skinData.dye;
-          skinValue[i + 33] = skinData.waist;
+          skinValue[i + 29] = skinData.shoe;
+          skinValue[i + 32] = skinData.waist;
+          skinValue[i + 33] = skinData.face;
+          skinValue[i + 34] = skinData.pet;
+          skinValue[i + 36] = skinData.wrist;
+          skinValue[i + 39] = [skinData.charm, skinData.charms];
         }
       }
     }
