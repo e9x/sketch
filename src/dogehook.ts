80b5a7f15efa4910986a3a35fab098efa73c6083
David Reed <reedswork@proton.me>
2025-10-14 22:29:58 -0400

    fix the script

--- src/dogehook.ts
+++ src/dogehook.ts
@@ -5,30 +5,36 @@ const window = getExposedWindow();
 let tokenPromiseResolve: (url: string) => void;
 
 const tokenPromise = new Promise<string>(resolve => tokenPromiseResolve = resolve);
-const ifr = document.createElement("iframe");
-ifr.src = location.href;
-ifr.style.display = "none";
-document.documentElement.append(ifr);
-// @ts-ignore
-const ifrFetch = ifr.contentWindow.fetch;
-Object.defineProperty(ifr.contentWindow, "fetch", {
-    get() {
-        // @ts-ignore
-        if (ifr.contentWindow?.windows?.length > 0) {
+
+
+let ifr: HTMLIFrameElement;
+
+function makeFrame() {
+    ifr = document.createElement("iframe");
+    ifr.src = location.href;
+    ifr.style.display = "none";
+    document.documentElement.append(ifr);
+    // @ts-ignore
+    const ifrFetch = ifr.contentWindow.fetch;
+    Object.defineProperty(ifr.contentWindow, "fetch", {
+        get() {
             // @ts-ignore
-            return function (url, init) {
-                if (url.includes("/seek-game")) {
-                    ifr.remove();
-                    tokenPromiseResolve(url);
-                    return;
-                }
+            if (ifr.contentWindow?.windows?.length > 0) {
                 // @ts-ignore
-                return ifrFetch.call(this, url, init);
-            };
+                return function (url, init) {
+                    if (url.includes("/seek-game")) {
+                        ifr.remove();
+                        tokenPromiseResolve(url);
+                        return;
+                    }
+                    // @ts-ignore
+                    return ifrFetch.call(this, url, init);
+                };
+            }
+            return ifrFetch;
         }
-        return ifrFetch;
-    }
-});
+    });
+};
 
 const _fetch = window.fetch;
 window.fetch = async function (url, init) {
@@ -38,20 +44,22 @@ window.fetch = async function (url, init) {
     return _fetch.call(this, url, init);
 };
 
+let addedFr = false;
+
 export const gameLoad = new Promise<void>((loaded) => {
     const observer = new MutationObserver((mutations) => {
         for (const mutation of mutations) {
+            if (!addedFr && document.documentElement) {
+                makeFrame();
+                addedFr = true;
+            }
+
             for (var i = 0; i < mutation.addedNodes.length; i++) {
                 const node = mutation.addedNodes[i] as HTMLScriptElement;
                 if (node.tagName === "SCRIPT") {
                     if (node.src.startsWith("https://krunker.io/static/index-")) {
                         node.remove();
                         loaded();
-
-                        // const game = downloadFileSync(serverUrl + "/game_1_4.js?" + Math.random().toString().slice(2));
-                        // window.addEventListener("load", () => {
-                        //     Function(id + "();\n\n" + game)();
-                        // }, 1000);
                     }
                 }
             }
